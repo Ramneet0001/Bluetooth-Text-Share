ef77cc4266258ebc53b0f14e1f4ddd1a10a51dcc
Tushar Verma <tverma@anviam.com>
2020-03-24 09:28:16 +0530

    Initial Commit

---
+++ BlueToothDataSend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueToothDataSend
+//
+//  Created by Apple on 24/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
